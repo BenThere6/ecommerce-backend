ee2b2158f8fb681db517375836546bae08554dae
BenThere <benjaminbirdsall@icloud.com>
2023-09-27 15:42:09 -0600

    complete associations

--- models/index.js
+++ models/index.js
@@ -3,21 +3,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id', // This is the foreign key in the Products table
-  onDelete: 'CASCADE', // This ensures that if a Category is deleted, associated Products are also deleted
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
   Category,
   Tag,
-  ProductTag,
+  ProductTag
 };
