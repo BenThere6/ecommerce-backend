18b86487d18c714533cc51ae32b9db46d2111f43
BenThere <benjaminbirdsall@icloud.com>
2023-09-27 15:39:59 -0600

    add products belong to category

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // This is the foreign key in the Products table
+  onDelete: 'CASCADE', // This ensures that if a Category is deleted, associated Products are also deleted
+});
 
 // Categories have many Products
 
